4b79da0e873de814e863a176177038b00f5258a3
Sanjay K <141281676+Sanjaykamaladoss@users.noreply.github.com>
2025-01-30 22:58:03 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -4,6 +4,15 @@ public class Factorial {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number :");
+int n = sc.nextInt();
+int Factorial=1;
+        for (int i = 1; i <=n; i++) {
+            Factorial*=i;
+            System.out.println("The facotrial of"+" "+n+" "+"is :"+Factorial);
+
+        }
 
 	}
 
