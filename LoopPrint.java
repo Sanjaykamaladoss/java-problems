5e56b32a8d36cee822fe84e54e81608bdf361614
Sanjay K <141281676+Sanjaykamaladoss@users.noreply.github.com>
2025-01-30 23:51:40 +0530

    Update LoopPrint.java

--- LoopPrint.java
+++ LoopPrint.java
@@ -5,7 +5,7 @@ public class LoopPrint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0;i<=20;i++) {
-			System.out.println("ALL THE BEST");
+			System.out.println(i+"ALL THE BEST");
 			}
 		
 
