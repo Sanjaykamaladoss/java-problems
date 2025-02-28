7bfc09be8eb75adbf2303fb382e06cc728436f2f
Sanjay K <141281676+Sanjaykamaladoss@users.noreply.github.com>
2025-02-28 19:18:12 +0530

    Update GreaterNumber.java

--- GreaterNumber.java
+++ GreaterNumber.java
@@ -19,7 +19,7 @@ public class GreaterNumber {
 	        	//condition for first number whether greater 
 	            System.out.println(num1 + " is greater than " + num2);
 	        } else if (num1 < num2) {
-	            System.out.println(num2 + " is greater than " + num1);
+	            System.out.print(num2 + " is greater than " + num1);
 	        } else {
 	            System.out.println("Both numbers are equal.");
 	        }
